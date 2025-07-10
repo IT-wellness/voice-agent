349eea5b7bbfb4fabe720db7e0816c73e9bbce9c
IT-wellness <itsupport@wellnessextract.com>
2025-07-10 14:14:34 +0530

    sdfdsfsdf

--- src/server.js
+++ src/server.js
@@ -6,12 +6,13 @@ import logger from './utils/logger.js';
 // HTTP Server
 const httpServer = app.listen(config.server.port, () => {
   logger.info(`HTTP server listening on port ${config.server.port}`);
+}).on('error', (err) => {
+  logger.error(`HTTP server error: ${err.message}`);
 });
 
 // WebSocket Server for Media
 const mediaSocketServer = new MediaSocketServer(config.server.wsPort);
 logger.info(`WebSocket server for media listening on port ${config.server.wsPort}`);
-
 // Graceful shutdown
 const shutdown = async () => {
   logger.info('Shutting down gracefully...');
@@ -25,3 +26,11 @@ const shutdown = async () => {
 
 process.on('SIGTERM', shutdown);
 process.on('SIGINT', shutdown);
+
+process.on('uncaughtException', (err) => {
+  logger.error(`Uncaught Exception: ${err.stack}`);
+});
+
+process.on('unhandledRejection', (reason, p) => {
+  logger.error(`Unhandled Rejection at: ${p}, reason: ${reason}`);
+});
